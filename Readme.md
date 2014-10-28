fd653d62ae6ebea72b6700cdd6ce5dc729681f37
Wendy Nichols <wendlelyn@gmail.com>
2014-10-28 08:33:24 -0400

    updated files

---
+++ Readme.md
@@ -0,0 +1,9 @@
+The Iron Yard Atlanta
+Fall 2014 Front End Class
+
+Assignment 13: Create a new project and use Handlebars in it.
+
+Libraries Used:
+Handlebars
+jQuery
+normalize
---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+  var library = {
+  albums : [
+    { album: '1989', artist: 'Taylor Swift' },
+    { album: 'Montevallo', artist: 'Sam Hunt'},
+    { album: 'Rise', artist: 'Trip Lee'},
+    { album: 'Black Veil Brides', artist: 'Black Veil Brides'},
+    { album: 'Forever Together', artist: 'Emblem3'}
+    ]
+  };
+
+    // Grabbing our template string
+  var template = $('#album').html();
+
+    // Creating our handlebars function
+  var rendered = Handlebars.templates['albums'];
+
+
+    // Append to my book
+  $('#albums').append( rendered(library) );
